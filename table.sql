ddbc9742b949adb787a63e3f58bac947e21592db
RavinduHarshana <ravinduharshana2001@gmail.com>
2023-10-12 00:44:18 +0530

    Create Lecturer Table

---
+++ table.sql
@@ -0,0 +1,11 @@
+-- Create Lecturer Table
+
+CREATE TABLE
+    Lecturer(
+        LecturerId VARCHAR (10),
+        LecName VARCHAR (50),
+        NIC INT (15),
+        DepID VARCHAR (10),
+        PRIMARY KEY(LecturerID)
+    );
+

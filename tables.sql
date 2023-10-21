3ef880db2c6e6a03f1fe2f281e0682d6afb80733
W.I.K.N.Jayathilaka <imalkesara2001@gmail.com>
2023-10-21 12:49:17 +0530

    create tables

---
+++ tables.sql
@@ -0,0 +1,40 @@
+ CREATE TABLE course
+    -> ( courseID VARCHAR(10),
+    -> Name VARCHAR(20),
+    -> Credit DECIMAL(3,2),
+    -> Week INT,
+    -> stuID VARCHAR(10),
+    -> DepID VARCHAR(5),
+    -> LecturerId VARCHAR(10),
+    -> FOREIGN KEY(stuID) REFERENCES student(stuID),
+    -> FOREIGN KEY(DepID) REFERENCES Department(DepID),
+    -> FOREIGN KEY(LecturerId) REFERENCES lecturer(LecturerId),
+    -> PRIMARY KEY(courseID));
+Query OK, 0 rows affected (0.02 sec)
+
+
+CREATE TABLE eligibility(
+    -> CAStatus VARCHAR(10),
+    -> courseID varchar(10),
+    -> stuID varchar(10),
+    -> FOREIGN KEY(courseID) REFERENCES course(courseID),
+    -> FOREIGN KEY(stuID) REFERENCES student(stuID),
+    -> PRIMARY KEY(CAStatus));
+Query OK, 0 rows affected (0.03 sec)
+
+CREATE TABLE notice(
+    -> noticeID VARCHAR(5),
+    -> title VARCHAR(10),
+    -> Description VARCHAR(25),
+    -> Date date,
+    -> stuID varchar(10),
+    -> LecturerId VARCHAR (10),
+    -> FOREIGN key(LecturerId) REFERENCES lecturer(LecturerId),
+    -> FOREIGN key(stuID) REFERENCES student(stuID),
+    -> PRIMARY KEY(noticeID));
+Query OK, 0 rows affected (0.03 sec)
+
+
+
+
+

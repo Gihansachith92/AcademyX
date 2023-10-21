3062e4519ad99e462d2c08be46e23d3824cf346f
Gihan_Sachith <gihansachith92@gmail.com>
2023-10-21 15:43:57 +0530

    add lecturer

--- tables.sql
+++ tables.sql
@@ -30,5 +30,16 @@ CREATE TABLE Assignment
     FOREIGN KEY (courseID) REFERENCES Course(courseID)  );
 
 
+-- Create Lecturer Table
+
+CREATE TABLE
+    Lecturer(
+        LecturerId VARCHAR (10),
+        LecName VARCHAR (50),
+        NIC INT (15),
+        DepID VARCHAR (10),
+        PRIMARY KEY(LecturerID)
+    );
+
 
 

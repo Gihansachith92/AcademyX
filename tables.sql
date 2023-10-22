2706acb0a57f7402402e0a13a7333e0b7eee0568
Niwantha N Senanayake <140209043+niwanthaniluka@users.noreply.github.com>
2023-10-22 12:52:03 +0530

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -161,4 +161,31 @@ INSERT INTO Lecturer VALUES
 
 
 
+/*
+INSERT INTO Student Values
+
+('TG/21/1011','proper',9032),
+('TG/21/1014','proper',3485),
+('TG/21/1020','proper',7129),
+('TG/21/1024','proper',6402),
+('TG/21/1027','proper',8973),
+('TG/21/1031','proper',5316),
+('TG/21/1036','proper',2657),
+('TG/21/1042','proper',4820),
+('TG/21/1045','proper',3901),
+('TG/21/1050','proper',8194),
+('TG/20/980','repeat',7265),
+('TG/20/972','repeat',1463),
+('TG/20/945','repeat',5738),
+('TG/19/866','repeat',9250),
+('TG/19/854','repeat',6709),
+('TG/18/678','suspended',2154),
+('TG/18/645','suspended',3496),
+('TG/17/415','suspended',6837) ;
+
+*/
+
+
+
+
 

ba2cf43a8e35495954f79b4a4d8c00a306865a95
Niwantha N Senanayake <140209043+niwanthaniluka@users.noreply.github.com>
2023-10-22 15:30:09 +0530

    update

--- table3.sql
+++ table3.sql
@@ -1,11 +1,11 @@
    -- Exam table creation
 
   CREATE TABLE Exam(
-        ExamID VARCHAR(10),
-        mid VARCHAR(10),
-        final VARCHAR(10),
-        marks VARCHAR(10),
-        stuID VARCHAR(10),
+        ExamID VARCHAR(20),
+        mid VARCHAR(20),
+        final VARCHAR(20),
+        marks VARCHAR(20),
+        stuID VARCHAR(20),
         PRIMARY KEY(ExamID),
         FOREIGN KEY (stuID) REFERENCES Student(stuID)
          );
@@ -16,7 +16,7 @@
    -- admin table creation
 
      CREATE TABLE admin(
-         adminID VARCHAR(5),
+         adminID VARCHAR(20),
          AdminName VARCHAR(20),
          NIC INT,
          PRIMARY KEY(adminID),
@@ -27,8 +27,8 @@
    -- Department table creation
 
      CREATE TABLE Department(
-        DepID VARCHAR(5) ,
-        name VARCHAR(20),
+        DepID VARCHAR(20),
+        name VARCHAR(30),
         PRIMARY KEY(DepID)
          );
 
@@ -38,22 +38,22 @@
 
 INSERT INTO Student Values
 
-('TG/21/1011','proper',9032),
-('TG/21/1014','proper',3485),
-('TG/21/1020','proper',7129),
-('TG/21/1024','proper',6402),
-('TG/21/1027','proper',8973),
-('TG/21/1031','proper',5316),
-('TG/21/1036','proper',2657),
-('TG/21/1042','proper',4820),
-('TG/21/1045','proper',3901),
-('TG/21/1050','proper',8194),
-('TG/20/980','repeat',7265),
-('TG/20/972','repeat',1463),
-('TG/20/945','repeat',5738),
-('TG/19/866','repeat',9250),
-('TG/19/854','repeat',6709),
-('TG/18/678','suspended',2154),
-('TG/18/645','suspended',3496),
-('TG/17/415','suspended',6837) ;
+('TG/2021/1011','proper',9032),
+('TG/2021/1020','proper',7129),
+('TG/2021/1014','proper',3485),
+('TG/2021/1024','proper',6402),
+('TG/2021/1027','proper',8973),
+('TG/2021/1031','proper',5316),
+('TG/2021/1036','proper',2657),
+('TG/2021/1042','proper',4820),
+('TG/2021/1045','proper',3901),
+('TG/2021/1050','proper',8194),
+('TG/2020/980','repeat',7265),
+('TG/2020/972','repeat',1463),
+('TG/2020/945','repeat',5738),
+('TG/2019/866','repeat',9250),
+('TG/2019/854','repeat',6709),
+('TG/2018/678','suspended',2154),
+('TG/2018/645','suspended',3496),
+('TG/2017/415','suspended',6837) ;
 

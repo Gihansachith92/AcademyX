efb33ee0ddd6d160863431eed8b01ccbc3d78b6b
W.I.K.N.Jayathilaka <imalkesara2001@gmail.com>
2023-10-22 15:26:57 +0530

    changed notice id

--- tables.sql
+++ tables.sql
@@ -1,13 +1,13 @@
  CREATE TABLE course
     ( 
-    courseID VARCHAR(10),
-    Name VARCHAR(20),
+    courseID VARCHAR(20),
+    CNames VARCHAR(100),
     Credit INT,
     Week INT ,
     Theory INT,
     Practical INT,
-    DepID VARCHAR(5),
-    LecturerId VARCHAR(10),
+    DepID VARCHAR(20),
+    LecturerId VARCHAR(20),
     FOREIGN KEY(stuID) REFERENCES student(stuID),
     FOREIGN KEY(DepID) REFERENCES Department(DepID),
     FOREIGN KEY(LecturerId) REFERENCES lecturer(LecturerId),
@@ -15,20 +15,20 @@
 
 CREATE TABLE eligibility(
      CAStatus VARCHAR(10),
-     courseID varchar(10),
-     stuID varchar(10),
+     courseID varchar(20),
+     stuID varchar(20),
      FOREIGN KEY(courseID) REFERENCES course(courseID),
      FOREIGN KEY(stuID) REFERENCES student(stuID),
      PRIMARY KEY(CAStatus));
 
 
 CREATE TABLE notice(
-    noticeID VARCHAR(5),
-    title VARCHAR(10),
+    noticeID VARCHAR(20),
+    title VARCHAR(20),
     Description VARCHAR(25),
     Date date,
-    stuID varchar(10),
-    LecturerId VARCHAR (10),
+    stuID varchar(20),
+    LecturerId VARCHAR (20),
     FOREIGN key(LecturerId) REFERENCES lecturer(LecturerId),
     FOREIGN key(stuID) REFERENCES student(stuID),
     PRIMARY KEY(noticeID));
@@ -91,7 +91,7 @@ VALUES(
 );
 
  CREATE TABLE admin(
-         adminID VARCHAR(5),
+         adminID VARCHAR(20),
          NIC INT,
          PRIMARY KEY(adminID),
          FOREIGN KEY (NIC) REFERENCES User(NIC)

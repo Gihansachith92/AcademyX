9589f192334b35a781878cb04c3101d391b4c39b
W.I.K.N.Jayathilaka <imalkesara2001@gmail.com>
2023-10-24 23:53:33 +0530

    ADD MEDICAL

--- exsample.sql
+++ exsample.sql
@@ -193,3 +193,23 @@ VALUES
 ("TG/2018/678"  , "ICT1212","DpICT",0),
 ("TG/2018/645"  , "ICT1212","DpICT",0),
 ("TG/2017/415"  , "ICT1212","DpICT",0);
+
+CREATE TABLE
+    Medical(
+        MeidcalID VARCHAR(20),
+        stuID VARCHAR(20),
+        courseID VARCHAR(20),
+        DepID VARCHAR(20),
+        Submitdate DATE,
+        Description VARCHAR(100),
+        FOREIGN KEY(courseID) REFERENCES Course(courseID),
+        FOREIGN KEY(stuID) REFERENCES Student(stuID),
+        FOREIGN KEY(DepID) REFERENCES Department(DepID),
+        PRIMARY KEY(MeidcalID));
+
+
+INSERT INTO MEDICAL VALUES
+("MED001","TG/2021/1014","ICT1233","DpICT","2023-07-20","UNA"),
+("MED002","TG/2021/1036","ICT1233","DpICT","2023-05-21","BADA RIDENWA"),
+("MED003","TG/2021/1011","ICT1212","DpICT","2023-05-11","OLUWA RIDENWA"),
+("MED004","TG/2021/1020","ICT1212","DpICT","2023-03-29","PUKA RIDNEWA");

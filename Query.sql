49bc21a2666736c8678f38dc3576a61b0705fb46
Gihan_Sachith <gihansachith92@gmail.com>
2023-10-27 22:16:00 +0530

    Create the StudentQuiz_Marks view

--- Query.sql
+++ Query.sql
@@ -5,3 +5,12 @@ SELECT DISTINCT e.stuid, e.courseid, e.depid, (e.quiz1 + e.quiz2 + e.quiz3 + e.M
 FROM exammarks e
 INNER JOIN student s ON e.stuid = s.stuid
 WHERE s.states != 'suspended';
+
+-- Create the StudentQuiz_Marks view
+CREATE VIEW StudentQuiz_Marks AS
+SELECT e.stuid, e.courseid, e.depid, 
+    (e.quiz1 + e.quiz2 + e.quiz3 - LEAST(e.quiz1, LEAST(e.quiz2, e.quiz3))) / 2 AS `Average_Quiz_Score`
+FROM exammarks e
+INNER JOIN student s ON e.stuid = s.stuid
+WHERE s.states != 'suspended';
+

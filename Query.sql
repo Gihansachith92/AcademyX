0e647ec91bd66ad293e1c6082a01741d1cdd6045
Gihan_Sachith <gihansachith92@gmail.com>
2023-10-27 23:04:13 +0530

    comment

--- Query.sql
+++ Query.sql
@@ -1,10 +1,10 @@
 -- Create the StudentCA_Marks view
 
-CREATE VIEW StudentCA_Marks AS
-SELECT DISTINCT e.stuid, e.courseid, e.depid, (e.quiz1 + e.quiz2 + e.quiz3 + e.MidPractical + e.MidTheory + e.Assesments) / 5 AS `CA_Marks`
-FROM exammarks e
-INNER JOIN student s ON e.stuid = s.stuid
-WHERE s.states != 'suspended';
+-- CREATE VIEW StudentCA_Marks AS
+-- SELECT DISTINCT e.stuid, e.courseid, e.depid, (e.quiz1 + e.quiz2 + e.quiz3 + e.MidPractical + e.MidTheory + e.Assesments) / 5 AS `CA_Marks`
+-- FROM exammarks e
+-- INNER JOIN student s ON e.stuid = s.stuid
+-- WHERE s.states != 'suspended';
 
 
 

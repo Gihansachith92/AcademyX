6854175442a5313197a1499784a4d97e25670b20
Gihan_Sachith <gihansachith92@gmail.com>
2023-10-28 00:58:27 +0530

    Create the StudentCA_Marks view

--- Query.sql
+++ Query.sql
@@ -1,11 +1,3 @@
--- Create the StudentCA_Marks view
-
--- CREATE VIEW StudentCA_Marks AS
--- SELECT DISTINCT e.stuid, e.courseid, e.depid, (e.quiz1 + e.quiz2 + e.quiz3 + e.MidPractical + e.MidTheory + e.Assesments) / 5 AS `CA_Marks`
--- FROM exammarks e
--- INNER JOIN student s ON e.stuid = s.stuid
--- WHERE s.states != 'suspended';
-
 
 
 
@@ -41,7 +33,7 @@ WHERE s.states != 'suspended';
 
 
 
--- Create the StudentCA_Marks view 
+-- Create the StudentCA_Marks view  level 1
 CREATE VIEW StudentCA_Marks AS
 SELECT e.stuid, e.courseid, e.depid,
              ((e.quiz1 + e.quiz2 + e.quiz3 - LEAST(e.quiz1, LEAST(e.quiz2, e.quiz3))) / 2) * 0.05 AS `Quiz_Score` ,
@@ -50,3 +42,45 @@ SELECT e.stuid, e.courseid, e.depid,
      FROM exammarks e
      INNER JOIN student s ON e.stuid = s.stuid
      WHERE s.states != 'suspended';
+
+
+-- Create the StudentCA_Marks view  level 2
+SELECT e.stuid, e.courseid, e.depid,
+       ((e.quiz1 + e.quiz2 + e.quiz3 - LEAST(e.quiz1, LEAST(e.quiz2, e.quiz3))) / 2) * 0.05 AS `Quiz_Score`,
+       (e.Assesments * 0.05) AS `Assessment_Mark`,
+       ((e.MidTheory + e.MidPractical) * 0.20) AS `MidMark`,
+       ((
+           ((e.quiz1 + e.quiz2 + e.quiz3 - LEAST(e.quiz1, LEAST(e.quiz2, e.quiz3))) / 2) * 0.05 +
+           (e.Assesments * 0.05) +
+           ((e.MidTheory + e.MidPractical) * 0.20)
+       )) as `CA_Marks`
+FROM exammarks e
+INNER JOIN student s ON e.stuid = s.stuid
+WHERE s.states != 'suspended';
+
+
+
+
+-- Create the StudentCA_Marks view 
+CREATE VIEW StudentCA_Marks AS
+SELECT e.stuid, e.courseid, e.depid,
+    ((e.quiz1 + e.quiz2 + e.quiz3 - LEAST(e.quiz1, LEAST(e.quiz2, e.quiz3))) / 2) * 0.05 AS `Quiz_Score`,
+    (e.Assesments * 0.05) AS `Assessment_Mark`,
+    ((e.MidTheory + e.MidPractical) * 0.20) AS `MidMark`,
+    (
+        ((e.quiz1 + e.quiz2 + e.quiz3 - LEAST(e.quiz1, LEAST(e.quiz2, e.quiz3))) / 2) * 0.05 +
+        (e.Assesments * 0.05) +
+        ((e.MidTheory + e.MidPractical) * 0.20)
+    ) as `CA_Marks`,
+    CASE
+        WHEN (
+            ((e.quiz1 + e.quiz2 + e.quiz3 - LEAST(e.quiz1, LEAST(e.quiz2, e.quiz3))) / 2) * 0.05 +
+            (e.Assesments * 0.05) +
+            ((e.MidTheory + e.MidPractical) * 0.20)
+        ) >= 15 THEN 'Eligible'
+        ELSE 'Not_Eligible'
+    END AS `Eligibility_Status`
+FROM exammarks e
+INNER JOIN student s ON e.stuid = s.stuid
+WHERE s.states != 'suspended';
+

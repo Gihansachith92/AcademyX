821b302fc7e67ad5aea0a2c0540971bb8e30a6b7
W.I.K.N.Jayathilaka <imalkesara2001@gmail.com>
2023-10-28 01:41:07 +0530

    😊

---
+++ viewExamCal.sql
@@ -0,0 +1,6 @@
+--Exam quizes total and precentage midterm for english 
+--CREATE VIEW ENGLISH_EXAM_
+
+SELECT QUIZ1,QUIZ2,QUIZ3,CONCAT((((QUIZ1+QUIZ2+QUIZ3)/300)*100) * 0.2) AS "20% QUIZES PRECENTAGE",Assesments,CONCAT(Assesments * 0.35) AS "35% Assesments",MidTheory,CONCAT(MidTheory * 0.45) AS "45% Assesments",CONCAT((MidTheory * 0.45)+(Assesments * 0.35)+(((QUIZ1+QUIZ2+QUIZ3)/300)*100) * 0.2) AS "##% ",StuID,courseID,DepID FROM exammarks
+where depID = "DpMUL" and courseID ="ENG1222";
+

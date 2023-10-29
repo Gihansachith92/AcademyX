8539dcd3b479e76d20488e5c1fadf1ca616d2767
RavinduHarshana <ravinduharshana2001@gmail.com>
2023-10-29 16:58:18 +0530

    ER Update

--- Quary.sql
+++ Quary.sql
@@ -1,8 +1,7 @@
-
 -- Calculate Percentage of attenedance quary test input
     SELECT (COUNT(Date)/15)*100 AS Percentage 
     FROM attendance
-    WHERE stuID = "TG/2021/1011" AND courseID = "ICT1253";
+    WHERE stuID = "TG/2021/1011" AND courseID = "ICT1212";
 
 -- Attendance Percentage Procedure
 CREATE PROCEDURE Attendance_Percentage(IN StuID VARCHAR(20),IN courseID VARCHAR(20))
@@ -14,12 +13,9 @@ END;//
 
 
 -- Call Procedure
+
 CALL Attendance_Percentage1 ('TG/2021/1014', 'ICT1253')//
 
 -- Create View retrive all notice
 CREATE VIEW notices AS
 SELECT * FROM notice ORDER BY Date DESC;
-
-
-SELECT StuID,courseID,(COUNT(Date)/15)*100 AS "80%Percentage" , IF((COUNT(Date)/15 * 100)>=80,"Eligible","Not Eligible") As "Eligiblity"
-FROM Attendance ;

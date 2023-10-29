05a355d2c9c1ccf3609b4e99094b07bca638593d
RavinduHarshana <ravinduharshana2001@gmail.com>
2023-10-30 04:53:33 +0530

    medical table update

--- table.sql
+++ table.sql
@@ -29,24 +29,29 @@
 --          );
 
 -- Craete Medical TABLE
-
 CREATE TABLE
     Medical(
         MeidcalID VARCHAR(20),
+        stuID VARCHAR(20),
+        courseID VARCHAR(20),
+        DepID VARCHAR(20),
         Submitdate DATE,
         Description VARCHAR(100),
-        PRIMARY KEY(MeidcalID)
-    );
+        FOREIGN KEY(courseID) REFERENCES Course(courseID),
+        FOREIGN KEY(stuID) REFERENCES Student(stuID),
+        FOREIGN KEY(DepID) REFERENCES Department(DepID),
+        PRIMARY KEY(MeidcalID));
 
 
 
-    INSERT INTO Medical (MeidcalID, Submitdate, Description)
-VALUES
-    ('MED001', '2023-07-26', 'Medical certificate for flu'),
-    ('MED002', '2023-06-22', 'Sick leave request'),
-    ('MED003', '2023-06-20', 'Doctor s note for a medical appointment'),
-    ('MED004', '2023-07-24', 'Medical leave due to injury'),
-    ('MED005', '2023-06-28', 'Prescription request');
+ 
+-- Medical data
+INSERT INTO MEDICAL VALUES
+("MED001","TG/2021/1014","ICT1233","DpICT","2023-07-20","Medical certificate for flu"),
+("MED002","TG/2021/1036","ICT1233","DpICT","2023-05-21","Sick leave request"),
+("MED003","TG/2021/1011","ICT1212","DpICT","2023-05-11","Doctor s note for a medical appointment"),
+("MED004","TG/2021/1020","ICT1212","DpICT","2023-03-29","Medical leave due to injury");
+
 
 -- Create coursemedical
 
@@ -59,6 +64,7 @@ CREATE TABLE
         FOREIGN KEY(courseID) REFERENCES Course(courseID),
         FOREIGN KEY(stuID) REFERENCES Student(stuID);
     );
+
 INSERT INTO coursemedical VALUES
     ('MED001','ICT1212','TG/2021/1024'),
     ('MED002','ICT1212','TG/2021/1014'),

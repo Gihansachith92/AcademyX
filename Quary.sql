99ef553a2d302174f755e0295aeb5aae1eb4c25a
RavinduHarshana <ravinduharshana2001@gmail.com>
2023-10-30 06:31:47 +0530

    Update Procedure

--- Quary.sql
+++ Quary.sql
@@ -1,31 +1,37 @@
--- -- Calculate Percentage of attenedance quary test input
---     SELECT (COUNT(Date)/15)*100 AS Percentage 
---     FROM attendance
---     WHERE stuID = "TG/2021/1011" AND courseID = "ICT1212";
-
--- -- Attendance Percentage Procedure
--- CREATE PROCEDURE Attendance_Percentage(IN StuID VARCHAR(20),IN courseID VARCHAR(20))
--- BEGIN
--- SELECT stuID,courseID,(COUNT(Date)/15)*100 AS Percentage 
--- FROM attendance
--- WHERE stuID = @stuID AND courseID = courseID;
--- END;//
-
-
-
--- Call Procedure
-
-CALL Attendance_Percentage1 ('TG/2021/1014', 'ICT1253')//
-
 -- Create View retrive all notice
 CREATE VIEW notices AS
 SELECT * FROM notice ORDER BY Date DESC;
 
 
--- attendance check procedure
-
-CREATE PROCEDURE Subject_attendance(IN StuID VARCHAR(20),IN courseID VARCHAR(20),IN DepID VARCHAR(20))
-BEIGN
-select stuID,courseID,DepID,sessionDate,sessionType,A_status,MeidcalID from attendance
-where stuID = @StuID AND courseID =@courseID AND DepID = @DepID;
+-- Subject  attendance check procedure
+CREATE PROCEDURE SubjectAttendance(
+    IN p_stuID VARCHAR(20),
+    IN p_courseID VARCHAR(20)
+)
+BEGIN
+    SELECT stuID, courseID, DepID, sessionDate, sessionType, A_status, MeidcalID
+    FROM attendance
+    WHERE stuID = p_stuID AND courseID = p_courseID;
 END;//
+
+-- call PROCEDURE
+CALL SubjectAttendance('TG/2021/1011', 'ENG1222');
+
+
+
+
+-- Attendance Percentage PROCEDURE
+
+CREATE PROCEDURE AttendancePercentage(
+    IN p_stuID VARCHAR(20),
+    IN p_courseID VARCHAR(20)
+)
+BEGIN
+    SELECT stuID, courseID, A_status, (COUNT(A_status) / 15 * 100) AS Percentage
+    FROM attendance
+    WHERE stuID = p_stuID AND courseID = p_courseID
+    GROUP BY A_status;
+END;//
+
+-- call PROCEDURE
+CALL CalculateAttendancePercentage('TG/2021/1011', 'ENG1222');

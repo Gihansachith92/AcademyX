0f1266554fb001e689933888efc1d563c10bb12c
RavinduHarshana <ravinduharshana2001@gmail.com>
2023-10-30 07:57:41 +0530

    Mid marks procedure

--- Quary.sql
+++ Quary.sql
@@ -2,6 +2,7 @@
 CREATE VIEW notices AS
 SELECT * FROM notice ORDER BY Date DESC;
 
+----------------------------------------------------------------------------------
 
 -- Subject  attendance check procedure
 CREATE PROCEDURE SubjectAttendance(
@@ -17,23 +18,35 @@ END;//
 -- call PROCEDURE
 CALL SubjectAttendance('TG/2021/1011', 'ENG1222');
 
+--------------------------------------------------------------------------------
 
 -- Attendance Percentage PROCEDURE
 
-CREATE PROCEDURE AttendancePercentage(
+
+CREATE PROCEDURE CalculateAttendancePercentage(
     IN p_stuID VARCHAR(20),
-    IN p_courseID VARCHAR(20)
+    IN p_courseID VARCHAR(20),
+    IN p_DepID VARCHAR(20)
 )
 BEGIN
-    SELECT stuID, courseID, A_status, (COUNT(A_status) / 15 * 100) AS Percentage
-    FROM attendance
-    WHERE stuID = p_stuID AND courseID = p_courseID
-    GROUP BY A_status;
-END;//
+    SELECT
+        stuID,
+        courseID,
+        A_status,
+        (COUNT(A_status) / 15 * 100) AS Percentage
+    FROM
+        attendance
+    WHERE
+        stuID = p_stuID
+        AND courseID = p_courseID
+        AND DepID = p_DepID
+    GROUP BY stuID, courseID, A_status;
+END;
 
 -- call PROCEDURE
-CALL CalculateAttendancePercentage('TG/2021/1011', 'ENG1222');
+CALL CalculateAttendancePercentage('TG/2021/1011', 'ENG1222','DpICT');
 
+--------------------------------------------------------------------------------
 
 
 -- CREATE VIEW attendance_eligibility AS
@@ -49,9 +62,9 @@ select * from attendance_eligibility where stuID = pstuID;
 END;
 
 -- call procedure
-CALL StudentallsubjectEligibility('TG/1021/1020');
+CALL StudentallsubjectEligibility('TG/2021/1020');
 
-
+--------------------------------------------------------------------------------
 
 -- check each subject quize marks
 
@@ -63,6 +76,7 @@ END;
 -- call procedure
 CALL CheckSubjectQuizemarks('ICT1222');
 
+--------------------------------------------------------------------------------
 
 -- check each student all quize marks using view procedure 
 CREATE PROCEDURE CheckStudentQuizemarks (IN pstuID VARCHAR(20))
@@ -72,3 +86,24 @@ END;
 
 -- call procedure
 CALL CheckStudentQuizemarks('TG/2021/1020');
+
+--------------------------------------------------------------------------------
+
+-- one student mid exam marks 
+CREATE PROCEDURE studentMid_Mark(IN pstuID VARCHAR(20) )
+BEGIN
+select * from studentmidterm_marks where stuID= pstuID;
+END;
+
+-- call procedure
+CALL studentMid_Mark('TG/2021/1020');
+
+--------------------------------------------------------------------------------
+-- subjectvise mid marks
+CREATE PROCEDURE subjectMid_Mark(IN pcourseID VARCHAR(20) )
+BEGIN
+select * from studentmidterm_marks where courseID= pcourseID;
+END;
+
+-- call procedure
+CALL subjectMid_Mark('ICT1222');

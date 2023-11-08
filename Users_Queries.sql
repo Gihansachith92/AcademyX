5a7110e819df307e09b109ecaf356ff7e680aa63
Niwantha N Senanayake <140209043+niwanthaniluka@users.noreply.github.com>
2023-11-09 02:05:58 +0530

    Update Users_Queries.sql

--- Users_Queries.sql
+++ Users_Queries.sql
@@ -1,30 +1,37 @@
 --Admin - With All privileges with Grant Option for all the tables in the database
 CREATE USER 'Admin'@'localhost' IDENTIFIED BY 'Ad1234';
 GRANT ALL PRIVILEGES ON AcademyX TO 'Admin'@'localhost' WITH GRANT OPTION;
-
+FLUSH PRIVILEGES;
 
 
 --Dean - With All privileges without Grant for all the tables in the database
 
 CREATE USER 'Dean'@'localhost' IDENTIFIED BY 'De1234';
 GRANT ALL PRIVILEGES ON AcademyX TO 'Dean'@'localhost';
-
+FLUSH PRIVILEGES;
 
 
 --Lecturer – All privileges without Grant and user creation for all the tables in the database
 CREATE USER 'Lecturer'@'localhost' IDENTIFIED BY 'Le1234';
 GRANT SELECT, INSERT, UPDATE, DELETE ON AcademyX TO 'Lecturer'@'localhost';
 
+FLUSH PRIVILEGES;
 
 
 --Technical Officer - Read, write and update permissions for attendance related tables/views
 CREATE USER 'TechnicalOfficer'@'localhost' IDENTIFIED BY 'Te1234';
 GRANT SELECT, INSERT, UPDATE ON TechnicalOfficer.attendance_eligibility TO 'TechnicalOfficer'@'localhost';
 
+FLUSH PRIVILEGES;
+
 
 
 --Student - Read permission for final attendance and final marks/Grades tables/views
-CREATE USER 'Student'@'localhost' IDENTIFIED BY 'St1234';
-GRANT SELECT ON project.attendance_eligibility TO 'Student'@'localhost';
-GRANT SELECT ON project.StudentFinal_Marks TO 'Student'@'localhost';
-GRANT SELECT ON project.StudentGradePoint TO 'Student'@'localhost';
+CREATE USER 'student'@'localhost' IDENTIFIED BY 'St1234';
+GRANT SELECT ON Academyx.attendance TO 'student'@'localhost';
+GRANT SELECT ON Academyx.attendance_eligibility TO 'student'@'localhost';
+GRANT SELECT ON AcademyX.exammarks TO 'student'@'localhost';
+GRANT SELECT ON Academyx.studentfinal_marks TO 'student'@'localhost';
+GRANT SELECT ON Academyx.studentgradepoint TO 'student'@'localhost';
+
+FLUSH PRIVILEGES;
